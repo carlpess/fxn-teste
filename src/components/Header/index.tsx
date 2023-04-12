de087935db309a987b1a4a8edbf13ab040c693d6
Carlos Pessoa <carlos.henriquepessoa15@gmail.com>
2023-04-11 22:21:33 -0300

    header estilizado, adicionada font-family

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,35 +6,43 @@ import ArrowIcon from '@/assets/arrow-icon.svg'
 
 export default function Header() {
     return (
-        <header>
-            <a href='#'>
-                <Image src={Logo} alt='logo' className={styles.logo} />
-            </a>
-            <nav>
-                <ul>
-                    <li>Antecipação</li>
-                    <li>Como funciona</li>
-                    <li>Conta Digital</li>
-                    <li>Sobre nós</li>
-                    <li>Contato</li>
-                </ul>
-                <div>
-                    <a href='#'>
-                        <Image
-                            src={BlogIcon}
-                            alt='blog'
-                        />
-                        Nosso blog
-                    </a>
-                    <a href="#" className={styles.arrow}>
-                        Antecipar agora
-                        <Image
-                            src={ArrowIcon}
-                            alt='arrow'
-                        />
-                    </a>
-                </div>
-            </nav>
+        <header className={styles.header}>
+            <div className={styles.containerHeader}>
+                <a href="#">
+                    <Image src={Logo} alt='logo' className={styles.logo} />
+                </a>
+                <nav className={styles.containerNavIN}>
+                    <ul>
+                        <li>Antecipação</li>
+                        <li>Como funciona</li>
+                        <li>Conta Digital</li>
+                        <li>Sobre nós</li>
+                        <li>Contato</li>
+                    </ul>
+                    <div className={styles.btns}>
+                        <a href="#" className={styles.blog}>
+                            <Image
+                                src={BlogIcon}
+                                alt='blog'
+                                className={styles.blogImage}
+                                width={23}
+                                height={23}
+                            />
+                            Nosso blog
+                        </a>
+                        <a href="#" className={styles.arrow}>
+                            Antecipar agora
+                            <Image
+                                src={ArrowIcon}
+                                alt='arrow'
+                                className={styles.arrowImage}
+                                width={23}
+                                height={23}
+                            />
+                        </a>
+                    </div>
+                </nav>
+            </div>
         </header>
     )
 }
